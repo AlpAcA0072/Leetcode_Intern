b3632a973699ab34a4472ad27aadc6550fd312b5
AlpAcA0072 <ALpAcA0072@gmail.com>
2024-03-13 11:09:27 +0800

    0020, 0021

---
+++ src/l0020.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class l0020 {
+    public boolean isValid(String s) {
+        char[] charArr = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char c : charArr) {
+            if ("([{".contains(String.valueOf(c))) {
+                stack.push(c);
+            } else if (c == '}') {
+                if (stack.isEmpty())
+                    return false;
+                else if (!(stack.pop() == '{'))
+                    return false;
+            } else if (c == ']') {
+                if (stack.isEmpty())
+                    return false;
+                else if (!(stack.pop() == '['))
+                    return false;
+            } else if (c == ')') {
+                if (stack.isEmpty())
+                    return false;
+                else if (!(stack.pop() == '('))
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
