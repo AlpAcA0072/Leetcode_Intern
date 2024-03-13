47bd2153c0ad6e4f3844b5db718a3f2dbedbce68
AlpAcA0072 <ALpAcA0072@gmail.com>
2024-03-13 15:40:09 +0800

    0053 whole

--- src/l0053.java
+++ src/l0053.java
@@ -1,6 +1,6 @@
 public class l0053 {
-
     // time O(n^2), space O(1) TLE
+    // Brute Force with memory
     class Solution {
         public int maxSubArray(int[] nums) {
             int maxSum = Integer.MIN_VALUE;
@@ -16,10 +16,38 @@ public class l0053 {
         }
     }
 
-    //
+    // time O(n), space O(1)
+    // sum(i, j) (from index i to j) = sum(0, j) - sum(0, i - 1)
+    // maxSum = maxSum(0, ?) - minSum(0, ?)
     class Solution2 {
         public int maxSubArray(int[] nums) {
-            return Integer.MIN_VALUE;
+            int maxResult = nums[0];
+            int minSum = 0;
+            int sum = 0;
+            for (int num : nums) {
+                sum += num;
+                maxResult = Math.max(maxResult, sum - minSum);
+                minSum = Math.min(minSum, sum);
+            }
+            return maxResult;
+        }
+    }
+
+    // divide and conquer
+    // time O(n*log(n)), space O(1)
+    // TODO
+    class Solution3 {
+        public int maxSubArray(int[] nums) {
+            return -1;
+        }
+    }
+
+    // dp
+    // time O(n), space O(1)
+    // TODO
+    class Solution4 {
+        public int maxSubArray(int[] nums) {
+            return -1;
         }
     }
 }
