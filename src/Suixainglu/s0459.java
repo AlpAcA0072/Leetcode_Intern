21b3ceaac300b46d10bccef5762b94509b5ee1bc
AlpAcA0072 <ALpAcA0072@gmail.com>
2024-04-14 13:30:16 +0800

    0459's three methods (including one-line method, KMP method and optimized KMP method) KMP Practice Problem

---
+++ src/Suixainglu/s0459.java
@@ -0,0 +1,71 @@
+package Suixainglu;
+
+import java.util.Arrays;
+
+public class s0459 {
+    class Solution1 {
+        public boolean repeatedSubstringPattern(String s) {
+            return (s + s).indexOf(s, 1) != s.length();
+        }
+    }
+
+    // 解的正确性证明: https://leetcode.cn/problems/repeated-substring-pattern/solutions/386481/zhong-fu-de-zi-zi-fu-chuan-by-leetcode-solution
+    class Solution2 {
+        public boolean repeatedSubstringPattern(String s) {
+            return kmp(s + s, s);
+        }
+
+        public boolean kmp(String query, String pattern) {
+            int n = query.length();
+            int m = pattern.length();
+            int[] fail = new int[m];
+            Arrays.fill(fail, -1);
+            for (int i = 1; i < m; ++i) {
+                int j = fail[i - 1];
+                while (j != -1 && pattern.charAt(j + 1) != pattern.charAt(i)) {
+                    j = fail[j];
+                }
+                if (pattern.charAt(j + 1) == pattern.charAt(i)) {
+                    fail[i] = j + 1;
+                }
+            }
+            int match = -1;
+            for (int i = 1; i < n - 1; ++i) {
+                while (match != -1 && pattern.charAt(match + 1) != query.charAt(i)) {
+                    match = fail[match];
+                }
+                if (pattern.charAt(match + 1) == query.charAt(i)) {
+                    ++match;
+                    if (match == m - 1) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+
+    // solution2的优化
+    class Solution3 {
+        public boolean repeatedSubstringPattern(String s) {
+            return kmp(s);
+        }
+
+        public boolean kmp(String pattern) {
+            int n = pattern.length();
+            int[] fail = new int[n];
+            Arrays.fill(fail, -1);
+            for (int i = 1; i < n; ++i) {
+                int j = fail[i - 1];
+                while (j != -1 && pattern.charAt(j + 1) != pattern.charAt(i)) {
+                    j = fail[j];
+                }
+                if (pattern.charAt(j + 1) == pattern.charAt(i)) {
+                    fail[i] = j + 1;
+                }
+            }
+            return fail[n - 1] != -1 && n % (n - fail[n - 1] - 1) == 0;
+        }
+    }
+}
